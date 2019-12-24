f1bb48408aa7d74da94381b8de4d2f23eb416f8e
134355 <38032758+134355@users.noreply.github.com>
2019-12-24 16:02:07 +0800

    Update builder.dart

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -2,4 +2,4 @@ import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
 import 'generator.dart';
 
-Builder imagePathBuilder(BuilderOptions options) => LibraryBuilder(ImagePathGenerator());
+Builder imagePathBuilder(BuilderOptions options) => LibraryBuilder(ImagePathGenerator(), generatedExtension: '.image.dart');
